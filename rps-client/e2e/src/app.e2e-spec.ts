bcd86cb03ad9b9873913bdb0de795dd94e0680f4
jenokiller <jenokiller1369@gmail.com>
2019-04-16 10:38:31 +0900

    E2Eテストの追加

--- rps-client/e2e/src/app.e2e-spec.ts
+++ rps-client/e2e/src/app.e2e-spec.ts
@@ -2,16 +2,52 @@ import {AppPage} from './app.po'
 import {browser, logging} from 'protractor'
 
 describe('workspace-project App', () => {
-  let page: AppPage
+  let page: AppPage;
+  let player1: string;
+  let player2: string;
 
   beforeEach(() => {
-    page = new AppPage()
-  })
+    page = new AppPage();
+    player1 = 'さとう';
+    player2 = '武藤';
+  });
 
   it('画面が表示される', () => {
-    page.navigateTo()
-    expect(page.getTitleText()).toEqual('じゃんけん')
-  })
+    page.navigateTo();
+    expect(page.getTitleText()).toContain('じゃんけん');
+  });
+
+  it('P1が勝つ', () => {
+    page.navigateTo();
+    page.playerNameInput(1, player1);
+    page.playerThrowHandInput(1, 'ROCK');
+    page.playerNameInput(2, player2);
+    page.playerThrowHandInput(2, 'SCISSORS');
+    page.clickButton();
+    expect(page.getResultText()).toContain(player1);
+    expect(page.getResultText()).toContain('wins!');
+  });
+
+  it('引き分けになる', () => {
+    page.navigateTo();
+    page.playerNameInput(1, player1);
+    page.playerThrowHandInput(1, 'ROCK');
+    page.playerNameInput(2, player2);
+    page.playerThrowHandInput(2, 'ROCK');
+    page.clickButton();
+    expect(page.getResultText()).toContain('tie!');
+  });
+
+  it('P2が勝つ', () => {
+    page.navigateTo();
+    page.playerNameInput(1, player1);
+    page.playerThrowHandInput(1, 'ROCK');
+    page.playerNameInput(2, player2);
+    page.playerThrowHandInput(2, 'PAPER');
+    page.clickButton();
+    expect(page.getResultText()).toContain(player2);
+    expect(page.getResultText()).toContain('wins!');
+  });
 
   afterEach(async () => {
     // Assert that there are no errors emitted from the browser
@@ -20,4 +56,4 @@ describe('workspace-project App', () => {
       level: logging.Level.SEVERE,
     } as logging.Entry))
   })
-})
+});
--- rps-client/e2e/src/app.po.ts
+++ rps-client/e2e/src/app.po.ts
@@ -1,16 +1,29 @@
 import {browser, by, element} from 'protractor'
 
 export class AppPage {
-
-  static playerThrowHandInput(playerNumber: number, throwHand: string) {
-    const throwHandInput = element(by.css('app-rps input[type=text]#p' + playerNumber + '-throw-hand'))
-    throwHandInput.sendKeys(throwHand)
-  }
   navigateTo() {
     return browser.get(browser.baseUrl)
   }
 
   getTitleText() {
     return element(by.css('app-rps h1')).getText()
+  }
+
+  playerNameInput(playerNumber: number, name: string) {
+    const nameInput = element(by.css('app-rps input[type=text]#p' + playerNumber + '-name'))
+    nameInput.sendKeys(name)
+  }
+
+  playerThrowHandInput(playerNumber: number, throwHand: string) {
+    const throwHandInput = element(by.css('app-rps select#p' + playerNumber + '-throw-hand option[value=' + throwHand + ']'))
+    throwHandInput.click()
+  }
+
+  clickButton() {
+    element(by.css('button')).click();
+  }
+
+  getResultText() {
+    return element(by.css('app-rps p')).getText()
   }
 }
